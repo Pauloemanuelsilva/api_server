15c6dfb0956a87607c46cde43ab73fe8ce669da0
PauloEmanuel <smariaclarice8@gmail.com>
2025-04-01 11:42:15 -0300

    fist commit

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+    //res.status(200).json(users);
+    res.send('ok, i am here.');
+});
+
+//ip 127.0.0.1:8080
+app.listen(8080);  
